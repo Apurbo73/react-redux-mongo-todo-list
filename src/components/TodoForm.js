cd8fb7676299efa3aeb3480d361a94737666671a
Apurbo73 <apurbodebnath895@gmail.com>
2023-07-20 23:05:31 +0600

    form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const TodoForm = () => {
-    return (
-        <div>
-            form
-        </div>
-    );
+  const [todo, setTodo] = useState("");
+  const handleSubmit = e => {};
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit} >
+        <input
+          type="text"
+          placeholder="enter todo title"
+          onChange={e => {
+            setTodo(e.target.value);
+          }}
+        /> <br />
+        <input type="submit" name="" id="" />
+      </form>
+    </div>
+  );
 };
 
 export default TodoForm;
